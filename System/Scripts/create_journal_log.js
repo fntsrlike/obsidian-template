d6e478b3debe2240372eefca2289e5a19b538050
Yuehu Lin <dev@fntsr.tw>
2024-06-14 15:23:07 +0800

    plugin(templater): support monthly and weekly on creating

--- System/Scripts/create_journal_log.js
+++ System/Scripts/create_journal_log.js
@@ -1,12 +1,69 @@
-const workspace = this.app.workspace
-const vault = this.app.vault
+const workspace = app.workspace
+const vault = app.vault
 let tp
 let tR = ''
+
+const journalTemplateName = {
+  daily: 'Daily Log Template',
+  weekly: 'Weekly Log Template',
+  monthly: 'Monthly Log Template',
+}
+
+const journalDir = {
+  daily: 'Journal/01. Daily Log/',
+  weekly: 'Journal/02. Weekly Log/',
+  monthly: 'Journal/03. Monthly Log/',
+}
+
+const journalFileNameFormatRegExp = {
+  daily: /^日誌：\d{4} 年 \d{2} 月 \d{2} 日 \([一二三四五六日]\)$/,
+  weekly: /^週誌：\d{4} 年 \d{2} 週$/,
+  monthly: /^月誌：\d{4} 年 \d{2} 月$/,
+}
+
+const throwError = (errorMessage) => {
+  console.error(errorMessage)
+  throw errorMessage
+}
 
 // static
 const testContentEmpty = (content) => {
   const isContentEmptyRegExp = /^\s*$/
   return isContentEmptyRegExp.test(content)
+}
+
+const testValidJournalFormat = (filename) => {
+  return !!getJournalType(filename)
+}
+
+const getJournalType = (filename) => {
+  if (journalFileNameFormatRegExp['daily'].test(filename)) {
+    return 'daily'
+  }
+
+  if (journalFileNameFormatRegExp['weekly'].test(filename)) {
+    return 'weekly'
+  }
+
+  if (journalFileNameFormatRegExp['monthly'].test(filename)) {
+    return 'monthly'
+  }
+
+  return false
+}
+
+const getJournalDir = (journalType) => {
+  if (journalType in journalDir) {
+    return journalDir[journalType]
+  }
+  throwError(`Error: undefined journal type! please check file name: '${journalFileName}'`)
+}
+
+const getJournalTemplateName = (journalType) => {
+  if (journalType in journalTemplateName) {
+    return journalTemplateName[journalType]
+  }
+  throwError(`Error: undefined journal type! please check file name: '${journalFileName}'`)
 }
 
 // depends on tp
@@ -43,13 +100,22 @@ const applyTemplateIfJournalEmpty = async (journalFile, templateFileName) => {
 
 const create_journal_log = async (_tp, options) => {
   tp = _tp
-  let { templateFileName, journalDir, journalFileName } = options
+  let { journalFileName } = options
+
+  if (!testValidJournalFormat(journalFileName)) {
+    throwError(`Error: undefined expect journal type! please check file name: '${journalFileName}'`)
+  }
+
+  const expectJournalType = getJournalType(journalFileName)
+  const templateFileName = getJournalTemplateName(expectJournalType)
+  const journalDir = getJournalDir(expectJournalType)
 
   const journalFilePath = `${journalDir}${journalFileName}.md`
   const journalFile = tp.file.find_tfile(journalFilePath);
   const isJournalFileExist = journalFile !== null
 
   const activeFilePath = tp.file.path(true)
+  const activeFileName = tp.file.title
   const activeFile = tp.file.find_tfile(activeFilePath);
 
   const isActiveFileEmpty = testContentEmpty(tp.file.content)
@@ -61,6 +127,14 @@ const create_journal_log = async (_tp, options) => {
   }
 
   if (isActiveFileAsJournal) {
+    return tR
+  }
+
+  if (testValidJournalFormat(activeFileName)) {
+    const journalType = getJournalType(activeFileName)
+    const dir = getJournalDir(journalType)
+    console.log(`${dir}${activeFileName}`)
+    await tp.file.move(`${dir}${activeFileName}`)
     return tR
   }
 
